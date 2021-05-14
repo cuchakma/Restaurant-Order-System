5646d307527fea6318ee5ef8ef3d51f55f8b231e
Cupid Chakma <greendaycu20@gmail.com>
2021-05-14 13:30:15 +0600

    css admin created

---
+++ admin/index.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title>Food Order Management Website</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <!-- Menu Section-->
+        <div class ="menu">
+            <div class="wrapper">
+                Menu Should Be Here
+            </div>
+        </div>
+        <!-- Menu Section-->
+
+
+        <!-- Main Content Section-->
+        <div class ="main-content">
+            <div class="wrapper">
+                Main Content Goes Here
+            </div>
+        </div>
+        <!-- Main Content Section-->
+
+        <!-- Footer Section-->
+        <div class ="footer">
+            <div class="wrapper">
+                Footer Content Goes Here
+            </div>
+        </div>
+        <!-- Footer Section-->
+    </body>
+</html>
