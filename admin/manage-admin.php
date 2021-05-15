cfdd96548bc8eaf42eef1721c73903484f63c2aa
Cupid Chakma <greendaycu20@gmail.com>
2021-05-15 10:41:04 +0600

    finished manage-admin.php

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -4,6 +4,28 @@
             <div class ="main-content">
                 <div class="wrapper">
                     <h1>Manage Admin</h1>
+                   <br>
+                   <br>
+                        <a href="#" class="btn-primary">Add Admin</a>
+                    <br /><br /><br />
+
+                    <table class="tbl-full">
+                        <tr>
+                            <th>S.N</th>
+                            <th>Full Name</th>
+                            <th>Username</th>
+                            <th>Actions</th>
+                        </tr>
+                        <tr>
+                            <td>Dummy 1</td>
+                            <td>Dummy 2</td>
+                            <td>Dummy 3</td>
+                            <td>
+                                <a href="#" class="btn-secondary">Update Admin</a>
+                                <a href="#" class="btn-danger">Delete Admin</a>
+                            </td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         <!-- Main Content Section-->
