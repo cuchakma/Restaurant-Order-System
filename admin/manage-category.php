ccbb262b15f6f09ae2642b02268e1e53eea3adf4
Cupid Chakma <greendaycu20@gmail.com>
2021-05-19 18:13:32 +0600

    added image in table category

--- admin/manage-category.php
+++ admin/manage-category.php
@@ -1,10 +1,11 @@
-<?php include 'partials/menu.php'; ?>
+<?php include( $_SERVER['DOCUMENT_ROOT']."/admin/partials/menu.php" ); ?>
 
 <div class="main-content">
     <div class="wrapper">
         <h1>Manage Category</h1>
         <br>
         <?php
+            $sl = 1;
             $add_category = isset( $_SESSION['add'] ) ? $_SESSION['add'] : '';
             echo $add_category;
             unset( $_SESSION['add'] );
@@ -16,21 +17,50 @@
         <table class="tbl-full">
             <tr>
                 <th>S.N</th>
-                <th>Full Name</th>
-                <th>Username</th>
+                <th>Title</th>
+                <th>Image</th>
+                <th>Featured</th>
+                <th>Active</th>
                 <th>Actions</th>
             </tr>
-            <tr>
-                <td>Dummy 1</td>
-                <td>Dummy 2</td>
-                <td>Dummy 3</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
+
+            <?php
+                /**
+                 * Fetch All Records From The Database
+                 */
+                $sql = "SELECT * FROM resto_category";
+                $result = mysqli_query($conn, $sql);
+                $count = mysqli_num_rows($result);
+                if( $count ) {
+                    while( $rows = mysqli_fetch_assoc($result) ) {
+                        $title      = isset( $rows['title'] ) ? $rows['title'] : '';
+                        $image_name = !empty( $rows['image_name'] ) ? '<img src = '.SITE_URL.'images/category/'.$rows['image_name'].' width = 100px >' : ('<div class="error">No Image Available</div>');
+                        $featured   = isset( $rows['featured'] ) ? $rows['featured'] : '';
+                        $active     = isset( $rows['active'] ) ?  $rows['active'] : '';
+                        ?>  
+                            <tr>
+                                <td><?php echo $sl++; ?></td>
+                                <td><?php echo $title; ?></td>
+                                <td><?php echo $image_name; ?></td>
+                                <td><?php echo $featured; ?></td>
+                                <td><?php echo $active; ?></td>
+                                <td>
+                                    <a href="#" class="btn-secondary">Update Category</a>
+                                    <a href="#" class="btn-danger">Delete Category</a>
+                                </td>
+                            </tr>
+                        <?php   
+                    }
+                } else {
+                    ?>
+                        <tr>
+                            <td colspan="6"><div class="error">No Category Added</div></td>
+                        </tr>
+                    <?php
+                }
+            ?>
         </table>
     </div>
 </div>
 
-<?php include 'partials/footer.php';?>
+<?php  include( $_SERVER['DOCUMENT_ROOT']."/admin/partials/footer.php"); ?>
