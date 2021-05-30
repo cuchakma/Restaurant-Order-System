b5dda27b4765b6bde87be2c1d9c096d14a04e3c0
Cupid Chakma <greendaycu20@gmail.com>
2021-05-30 19:25:42 +0600

    added dashboard values

--- admin/index.php
+++ admin/index.php
@@ -12,27 +12,49 @@
                 ?>
                 <br><br>
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+
+                    <?php 
+                        $sql    = "SELECT * FROM resto_category";
+                        $result = mysqli_query($conn, $sql);
+                        $rows   = mysqli_num_rows($result);
+                    ?>
+
+                    <h1><?php echo $rows; ?></h1>
                     <br/>
                     Categories
                 </div>
 
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+                    <?php 
+                        $sql2    = "SELECT * FROM resto_food";
+                        $result2 = mysqli_query( $conn, $sql2 );
+                        $rows2    = mysqli_num_rows($result2); 
+                    ?>
+                    <h1><?php echo $rows2;?></h1>
                     <br/>
-                    Categories
+                    Foods
                 </div>
 
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+                <?php
+                     $sql3    = "SELECT * FROM resto_order";
+                     $result3 = mysqli_query( $conn, $sql3 );
+                     $rows3   = mysqli_num_rows( $result3 ); 
+                ?>
+                    <h1><?php echo $rows3; ?></h1>
                     <br/>
-                    Categories
+                    Total Orders
                 </div>
 
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+                <?php 
+                    $sql4                = "SELECT SUM(price) as total from resto_order WHERE status = 'Delivered' ";
+                    $result4             = mysqli_query( $conn, $sql4 );
+                    $total_from_database = mysqli_fetch_assoc($result4);
+                ?>
+                    <h1><?php echo !empty( $total_from_database['total'] ) ? '$'.$total_from_database['total'] : 0; ?></h1>
                     <br/>
-                    Categories
+                    Revenue Generated
                 </div>
 
                 <div class="clearfix">
