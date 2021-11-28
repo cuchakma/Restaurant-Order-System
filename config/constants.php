7de035323acba22ad92758e0cdcde13750f91940
Cupid Chakma <wpdev@WPDev-Mac-Sabuz.localdomain>
2021-11-28 10:35:48 +0600

    mod

--- config/constants.php
+++ config/constants.php
@@ -6,8 +6,8 @@ session_start();
 
 define('SITE_URL', 'http://restaurant-system.test/');
 define('HOST', "localhost");
-define('USER', "debian-sys-maint");
-define('PASSWORD', "dYy3AmKg4rSh8qPJ");
+define('USER', "root");
+define('PASSWORD', "01906759899");
 define('DB_NAME', "food-resto");
 
 $conn = new mysqli(HOST, USER, PASSWORD, DB_NAME);
